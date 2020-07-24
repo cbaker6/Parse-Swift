5e6875dce40936066532da4d371365ff435e25fd
Corey's iMac <coreyearleon@icloud.com>
2020-07-23 21:30:18 -0400

    Update

--- Sources/ParseSwift/Objects Protocols/ObjectType.swift
+++ Sources/ParseSwift/Objects Protocols/ObjectType.swift
@@ -182,7 +182,6 @@ public extension ObjectType {
         return try saveCommand().execute(options: options)
     }
 
-    @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
     func saveAsync(options: API.Options, completion: @escaping (Self?, ParseError?) -> Void) {
         saveCommand().executeAsync(options: options, completion: completion)
     }
